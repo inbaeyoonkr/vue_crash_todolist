0c3c2ecda5afb93305d47b099aa58c08bfebf48f
ringo2455 <iby2455@gmail.com>
2020-07-20 22:42:02 +0900

    Learned how to handled data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import Vue from "vue";
+Vue.config.devtools = true;
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        { id: "1", title: "Todo 1", completed: true },
+        { id: "2", title: "Todo 2", completed: false },
+        { id: "3", title: "Todo 3", completed: true }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #bada55;
+  padding: 10px;
+  border-bottom: 2px solid black;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: red;
+  border-radius: 50%;
+  border: none;
+  float: right;
+  padding: 5px 9px;
+  color: white;
+}
+</style>
